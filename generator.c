4b539aa9ad132ec39b7f73f3b451c36f20891af5
alexwhitney90 <alexwhitney90@gmail.com>
2019-02-11 11:28:06 -0500

    initial commit

---
+++ generator.c
@@ -0,0 +1,102 @@
+
+/*
+
+Generator
+
+Your generator program writes randomly generated integers to standard output (by default), or to a file (if specified by the
+user). The integers themselves should be preceded by the number of integers that will follow, should appear one integer
+per output line, separated only by a single newline character (‘\n’). This specification is intended to match the input
+format that the sorter program expects. The number of integers and the range within which they fall are specified on the
+command line (see below), or with default values (also specified below). Your generator program should be compatible
+with your sorter program in the sense that your sorter should successfully read and sort integers produced by your
+generator. 
+
+Interface, Options, and Parameters
+
+Your programs should support the following interfaces:
+
+prog1sorter [-u] [-n <num-integers>] [-m <min-int>] [-M <max-int>]
+[-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>]
+
+prog1generator [-u] [-n <num-integers>] [-m <min-int>] [-M <max-int>]
+[-s <seed>] [-o <output-file-name>]
+
+Brackets indicate that all the arguments are optional; your program should support a replaceable default for each, as
+specified and described below. The user may specify options in any order. The meaning of each argument follows:
+
+-u
+	Print a usage string for your program on stderr and then exit. If –u appears anywhere on the command line, the
+	program should ignore all other arguments, print the usage string, and exit.
+
+-n <num-integers>
+	Your generator program should generate <num-integers> random integers.
+
+	Your sorter program should read (at most) the first <num-integers> values from the input file (or
+	from standard input), sort them, and write them out to the output file (or standard output).
+	If the input file (or stdin) contains more than <num-integers> values, your program should simply
+	ignore the rest.
+
+	For both programs, the minimum value for <num-integers> is 0. The default value for <numintegers> is 100. There is no maximum value for <num-integers>. If the user specifies a negative
+	value for <num-integers>, the program should print an informative error message to stderr and
+	exit immediately.
+
+-m <min-int>
+	Your generator program should generate integers no smaller than <min-int>.
+
+	Your sorter program should check for integers in the input that have a value less than <min-int>, and
+	halt with an informative error message as soon as the first one appears.
+
+	The default and minimum acceptable value for <min-int> is 1.
+
+-M <max-int>
+	Your generator program should generate integers no larger than <max-int>.
+
+	Your sorter program should check for integers in the input have a value greater than <max-int>, and
+	halt with an informative error message as soon as the first one appears.
+
+	The default value for <max-int> is 255. The maximum value for <max-int> is 1,000,000. <maxint> must not be less than <min-int>.
+
+-s <seed>
+	By default, your generator program should seed the random number generator with a value derived from
+	reading the system clock. If the user specifies the –s option, then your program should instead seed the
+	random number generator with <seed>. You may assume that <seed> is an unsigned long (and pass it
+	directly to srand()). (When writing code that produces pseudo-random values for testing, seeding the
+	random number generator with a time value is good practice. When debugging your code, you often want
+	the same random values to be produced for every run, so that the same behavior occurs each run. This
+	option allows you to select whether the numbers are pseudo-random or deterministic, on the command
+	line.)
+
+	Note that the –s option is not allowed for the sorter program.
+
+-i <input-file-name>
+	By default, your sorter program should read input from stdin. If –i is specified on the command line, it
+	should instead read input from the file named <input-file-name>.
+
+	Note that the –i option is not supported by the generator program.
+
+-o <output-file-name>
+	By default, your programs should write output to stdout. If – o is specified on the command line, it
+	should instead write output to the file named <output-file-name>. Your programs should
+	overwrite the contents of <output-file-name> if it exists when your program is invoked.
+
+-c <count-file-name>
+	As described above, your sorter should write the ASCII value and a count of the number of instances of
+	each letter in the Unix username of the person running the program. (Do not hardcode your own userid
+	into your program!) By default, your sorter program should write these values and counts to stdout. If
+	the user specifies the –c option, then your program should write the counts to <count-file-name>.
+	
+	Note that the generator program does not support the –c option.
+
+If the user specifies an option that your program does not support (-z for example), your program should print the
+usage string to standard error (stderr), and exit. If the user does not specify an argument where one is expected,
+the program should likewise report the problem, print the usage string to stderr, and then exit. You may assume
+that file names do not begin with a dash, and that the user specifies no negative numbers on the command line.
+Thus, you may assume that every command line option that begins with a dash (‘-‘) corresponds to an option to
+your program. 
+
+*/
+
+int main(int argc, char **argv)
+{
+	
+}
---
+++ sorter.c
@@ -0,0 +1,146 @@
+//sorter.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+
+Sorter
+
+The sorter reads a sequence of integers from standard input or from a file and outputs three different things:
+	• the sorted values, to standard output (stdout) by default, or to an output file (if one is specified by the user)
+	• a report about the counts of a few of the integers in the input, written to standard output by default, or to an output
+	file if one is specified by the user
+	• the elapsed time of the program, written to standard error (stderr).
+
+To sort, you should call the qsort() C library function. Read the man pages or online documentation to determine how
+to prepare the data for qsort(), and how to call the function. Using new library and system call functions is
+fundamentally important to becoming an effective systems programmer. Sorted values should appear one per line of
+output, in text format. Duplicate values in the input should appear in the output.
+
+Your program should count and report the number of occurrences of integers that correspond to the ASCII value of letters
+in the Linux userid of the person running the program. An example report for user mlewis appears below.
+
+m 109 5
+l 108 3
+e 101 6
+w 119 7
+i 105 2
+s 115 5
+
+The output should appear exactly as above; the userid characters (no matter whether they are lowercase letters, capital
+letters, or digits) appear in the order they appear in the userid, one per line. Each character is followed on each line by the
+ASCII value of the letter and then a count of the number of times that ASCII value appeared in the input. One tab
+character (‘\t’) should separate the letter and the ASCII value, and one more should separate the ASCII value and the
+count. Lines should contain no trailing whitespace, other than a single newline character (‘\n’). Repeated letters in the
+userid should be repeated in the output. For example:
+
+n 110 69
+e 101 58
+l 108 66
+l 108 66
+y 121 61
+4 52 53
+
+Sometimes we will build programs that check your output automatically. If it is not formatted exactly according to
+specifications, then either we have to take off way too many points, or grading becomes much more difficult. Let’s get
+used to specifying (me) and producing (you) precisely formatted output.
+
+Whether the program uses files or standard input and output depends on command line options, as described in more
+detail under the program command line options below.
+
+Your program should also report the time elapsed, in seconds (with a decimal part that includes precision to the
+microsecond) of your program. Timing should be done within your code, not from the shell. Start the timing after parsing
+command line arguments, and stop the timing after writing the last bit of output (other than time elapsed). Report the
+elapsed time on stderr, not on stdout.
+
+The positive integers to be sorted appear as input, one per line, in text format (i.e. not binary). Text format can be read
+directly by scanf() (among other functions), and written using fprintf() (among other functions).
+
+You may assume that the input contains valid positive integers only, and ends with a Control-D, the Linux end-of-file
+(EOF) character. Your program should read until EOF or the number of specified integers is reached (see -n below),
+whichever comes first.
+
+Please do not prompt the user for values; you should assume that the user knows how to use the program. You may
+include instructions after printing a “usage string” in response to “prog1sort –u” if you wish.
+
+*/
+
+int main(int argc, char **argv)
+{
+	int opt, numInts, minInt, maxInt;
+	char[] inputFile, outputFile, countFile;
+	bool numIntsExists = false;
+	bool maxIntExists = false;
+	bool minIntExists = false;
+	bool inputFileExists = false;
+	bool outputFileExists = false;
+	bool countFileExists = false;
+	while((opt = getopt(argc, argv, ":if:lrx")) != -1)  
+    {  
+        switch(opt)  
+        {
+			case 'u':
+				fprintf(stderr, "prog1sorter [-u] [-n <num-integers> [-m <min-int> [-M <max-int>] [-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>]");
+				system.exit(0);
+			case 'n':
+				numInts = optarg;
+				numIntsExists = true;
+				break;
+			case 'm':
+				minInt = optarg;
+				minIntExists = true;
+				break;
+			case 'M':
+				maxInt = optarg;
+				maxIntExists = true;
+				break;
+			case 'i':
+				inputFile = optarg;
+				inputFileExists = true;
+				break;
+			case 'o':
+				outputFile = optarg;
+				outputFileExists = true;
+				break;
+			case 'c':
+				countFile = optarg;
+				countFileExists = true;
+				break; 
+            case ':':  
+                fprintf(stderr, "prog1sorter [-u] [-n <num-integers> [-m <min-int> [-M <max-int>] [-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>]");
+                system.exit(0);
+            case '?':  
+                fprintf(stderr, "prog1sorter [-u] [-n <num-integers> [-m <min-int> [-M <max-int>] [-i <input-file-name>] [-o <output-file-name>] [-c <count-file-name>]");
+                system.exit(0); 
+        }  
+    }  
+      
+    // optind is for the extra arguments 
+    // which are not parsed 
+    for(; optind < argc; optind++)
+    {      
+        fprintf(stderr, "extra arguments: %s\n", argv[optind]);  
+    }
+    
+    if(inputFileExists == true)
+    {
+		if(numIntsExists == true)
+		{
+			int* numList = malloc(numInts*sizeof(int));
+			for(int i = 0; i < numInts; i++)
+			{
+				
+			}
+		}
+	}
+	
+	FILE* f;
+	f = popen("users");
+	
+	
+	
+	return 0;
+}
+
+
